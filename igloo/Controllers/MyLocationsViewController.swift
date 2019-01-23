88dae9afac611c30233eab67fb2260df054c8239
FinchFeng <329650437@qq.com>
2019-01-23 16:26:22 +0800

    除了VisitedNote MylocationVC MylocationModel全部完工

--- igloo/Controllers/MyLocationsViewController.swift
+++ igloo/Controllers/MyLocationsViewController.swift
@@ -28,65 +28,105 @@ class MyLocationsViewController: UIViewController {
     //MARK:Outlet
     @IBOutlet weak var locationTableView: MarsTableView!
     
+    func reloadTableViewData() {
+        //配置tableView M->C->V
+        locationTableView.setDataIn(locationDataArray:dataArrayForTableView)
+    }
+    
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //储存Image到本地图片池
-//        let image = #imageLiteral(resourceName: "locationTestImage")
-//        LocalImagePool.set(image: image, url: "imageTest")
-        //配置tableView
-        locationTableView.setDataIn(locationDataArray:dataArrayForTableView)
+        reloadTableViewData()
     }
     
     //MARK:functions
     
+    
     func hadLogin()  {
-        print("hadLoginFrom MylocationViewController")
         model.loginHander()
         //标记下一次展现这个页面需要重新刷新TableView
     }
     
-    @IBAction func buttonTaped() {
-        //测试成功
-//        deleteLocation()
-        testAddNewLocationData()
-//        deleteLocation()
-        print(LoginModel.owenLocationIDArray)
-        print(model.localLocationDataArray)
-//        print(model.localLocationDataArray)
-//          locationTableView.addCell(data: data)
-//        locationTableView.deleteCell(index:0)
-//        locationTableView.setDataIn(locationDataArray: [data])
-//        editLocationData()
+    func addLocation(data:LocationInfoLocal) {
+        //更改Model
+        model.addLocationInfo(data: data)
+        reloadTableViewData()
     }
     
-    func testAddNewLocationData() {
-        let rank1 = LocationInfoRank1(locationName: "Beef Noodle" ,iconKindString: "Restaurant" ,locationDescription: "A lots of beef" ,locationLatitudeKey: 37.334922 ,locationLongitudeKey: -122.009033 ,isPublic: true ,locationLikedAmount: 10 ,VisitedNoteID: [])
-        let rank2Data = LocationInfoRank2(locationName: "Beef Noodle" ,locationInfoWord: "nearby my home" ,locationLikedAmount: 10 ,locationInfoImageURL: "nil" )
-        let locationData = LocationInfoLocal(locationID: "4", rank1Data: rank1, rank2Data: rank2Data, visitedNoteArray: [])
-        model.addLocationInfo(data: locationData)
+    func deleteLocation(index:Int)  {
+        let id = model.localLocationDataArray[index].locationID
+        model.deleteLocaitonInfo(id: id)
+        reloadTableViewData()
     }
-
-    func editLocationData() {
-        let rank1 = LocationInfoRank1(locationName: "Noodles" ,iconKindString: "Restaurant" ,locationDescription: "A lots of beef" ,locationLatitudeKey: 37.334922 ,locationLongitudeKey: -122.009033 ,isPublic: true ,locationLikedAmount: 10 ,VisitedNoteID: [])
-        let rank2Data = LocationInfoRank2(locationName: "Beef Noodle" ,locationInfoWord: "nearby my home" ,locationLikedAmount: 10 ,locationInfoImageURL: "nil" )
-        let newData = LocationInfoLocal(locationID: "3", rank1Data: rank1, rank2Data: rank2Data, visitedNoteArray: [])
-        model.editLocationInfo(newData: newData, key: Constants.isPublic, value: "false")
+    
+    func changeLocationData(newData: LocationInfoLocal, key: String, value: String) {
+        model.editLocationInfo(newData: newData, key: key, value: value)
+        reloadTableViewData()
     }
-
-    func deleteLocation() {
-        model.deleteLocaitonInfo(id: "3")
-        
+    
+    //MARK:测试区
+    
+    @IBAction func buttonTaped() {
+                let rank1 = LocationInfoRank1(locationName: "BNoodles" ,iconKindString: "Restaurant" ,locationDescription: "A lots of beef" ,locationLatitudeKey: 37.334922 ,locationLongitudeKey: -122.009033 ,isPublic: true ,locationLikedAmount: 10 ,VisitedNoteID: [])
+                let rank2Data = LocationInfoRank2(locationName: "Beef Noodle" ,locationInfoWord: "nearby my home" ,locationLikedAmount: 10 ,locationInfoImageURL: "nil" )
+                let locationData = LocationInfoLocal(locationID: "4", rank1Data: rank1, rank2Data: rank2Data, visitedNoteArray: [])
+//                addLocation(data: locationData)
+        print(LoginModel.login)
+        print(LoginModel.owenLocationIDArray)
+        print(model.localLocationDataArray)
+//        deleteLocation(index: 0)
+        changeLocationData(newData: locationData, key: Constants.locationName, value: "BNoodles")//两个都要更改
     }
-
-
-    override func viewWillDisappear(_ animated: Bool) {
-        //注意要滑动TableView到顶端
-        super.viewWillDisappear(animated)
-    }
+    
+    
+    
+    //location的更改
+    
+    
+    
+    
+//    func testAddNewLocationData() {
+//        let rank1 = LocationInfoRank1(locationName: "Beef Noodle" ,iconKindString: "Restaurant" ,locationDescription: "A lots of beef" ,locationLatitudeKey: 37.334922 ,locationLongitudeKey: -122.009033 ,isPublic: true ,locationLikedAmount: 10 ,VisitedNoteID: [])
+//        let rank2Data = LocationInfoRank2(locationName: "Beef Noodle" ,locationInfoWord: "nearby my home" ,locationLikedAmount: 10 ,locationInfoImageURL: "nil" )
+//        let locationData = LocationInfoLocal(locationID: "4", rank1Data: rank1, rank2Data: rank2Data, visitedNoteArray: [])
+//        model.addLocationInfo(data: locationData)
+//    }
+//
+//    func editLocationData() {
+//        let rank1 = LocationInfoRank1(locationName: "Noodles" ,iconKindString: "Restaurant" ,locationDescription: "A lots of beef" ,locationLatitudeKey: 37.334922 ,locationLongitudeKey: -122.009033 ,isPublic: true ,locationLikedAmount: 10 ,VisitedNoteID: [])
+//        let rank2Data = LocationInfoRank2(locationName: "Beef Noodle" ,locationInfoWord: "nearby my home" ,locationLikedAmount: 10 ,locationInfoImageURL: "nil" )
+//        let newData = LocationInfoLocal(locationID: "3", rank1Data: rank1, rank2Data: rank2Data, visitedNoteArray: [])
+//        model.editLocationInfo(newData: newData, key: Constants.isPublic, value: "false")
+//    }
+//
+//    func deleteLocation() {
+//        model.deleteLocaitonInfo(id: "3")
+//
+//    }
+//
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        //注意要滑动TableView到顶端
+//        super.viewWillDisappear(animated)
+//    }
 
 }
 //    let dataArray:[(LocationInfoRank2,LocationInfoRank3)] = []
 //    let data = (LocationInfoRank2(locationName:"环岛路栈桥",locationInfoWord:"厦门 亚洲海湾",
 //                                  locationLikedAmount:12,locationInfoImageURL:"imageTest"),
 //                LocationInfoRank3(locationLatitudeKey: 37.334922,locationLongitudeKey:-122.009033,iconKindString:"Views"))
+
+//测试成功
+//        deleteLocation()
+//        testAddNewLocationData()
+//        deleteLocation()
+//        print(LoginModel.owenLocationIDArray)
+//        print(model.localLocationDataArray)
+//        print(model.localLocationDataArray)
+//          locationTableView.addCell(data: data)
+//        locationTableView.deleteCell(index:0)
+//        locationTableView.setDataIn(locationDataArray: [data])
+//        editLocationData()
+//储存Image到本地图片池
+//        let image = #imageLiteral(resourceName: "locationTestImage")
+//        LocalImagePool.set(image: image, url: "imageTest")
--- igloo/View/MarsTableView.swift
+++ igloo/View/MarsTableView.swift
@@ -26,17 +26,18 @@ class MarsTableView: UITableView,UITableViewDelegate,UITableViewDataSource {
     }
     
     //MARK:添加Cell或者删除Cell 使用之前TableView需要处于最顶部
-    func addCell(data:(rank2:LocationInfoRank2,rank3:LocationInfoRank3)) {
-        locationDataArray.insert(data, at: 0)
-        //执行动画
-        let firstCellIndex = IndexPath(row: 0, section: 0)
-        self.insertRows(at: [firstCellIndex], with: UITableView.RowAnimation.fade)
-    }
-    
-    func deleteCell(index:Int)  {
-        locationDataArray.remove(at: index)
-        self.deleteRows(at: [IndexPath(row: index, section: 0)], with: .none)
-    }
+//    func addCell(data:(rank2:LocationInfoRank2,rank3:LocationInfoRank3)) {
+//        locationDataArray.insert(data, at: 0)
+//        //执行动画
+//        let firstCellIndex = IndexPath(row: 0, section: 0)
+//        self.insertRows(at: [firstCellIndex], with: UITableView.RowAnimation.fade)
+//    }
+//
+//    func deleteCell(index:Int)  {
+//        locationDataArray.remove(at: index)
+//        reloadData()
+////        self.deleteRows(at: [IndexPath(row: index, section: 0)], with: .none)
+//    }
     
     //MARK:高度问题,与屏幕宽度成比例
     
