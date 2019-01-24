df31cd7fac6f9184364adab61d67582dc490136b
FinchFeng <329650437@qq.com>
2019-01-24 21:10:53 +0800

    添加新的GreatLocationInfoVIewController

---
+++ igloo/Controllers/GreatLocationInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreatLocationInfoViewController.swift
+//  igloo
+//
+//  Created by 冯奕琦 on 2019/1/24.
+//  Copyright © 2019 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class GreatLocationInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
