30baab9e20341e9726f1179d764c91ba849add81
FinchFeng <329650437@qq.com>
2019-02-02 08:12:43 +0800

    LocationChosen即将实现

--- igloo/Controllers/AddNewStuffVC/AddNewLocationViewController.swift
+++ igloo/Controllers/AddNewStuffVC/AddNewLocationViewController.swift
@@ -34,6 +34,11 @@ class AddNewLocationViewController: UIViewController,UITextFieldDelegate {
         return [iconImageButton1,iconImageButton2,iconImageButton3,iconImageButton4,iconImageButton5,iconImageButton6,iconImageButton7,iconImageButton8,iconImageButton9,iconImageButton10,iconImageButton11,iconImageButton12,iconImageButton13,iconImageButton14]
     }
     //其他
+    @IBOutlet weak var mapLocationImageLocationInfoStringLabel: UITextField!
+    @IBOutlet weak var mapLocationImageVIew: UIImageView!
+    @IBOutlet weak var mapLocationImageButton: UIButton!
+    @IBOutlet weak var isPublicSwitch: UISwitch!
+    @IBOutlet weak var iconLocationImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +62,13 @@ class AddNewLocationViewController: UIViewController,UITextFieldDelegate {
         let iconData = Constants.getIconStruct(image:image)!
         currentIconString = iconData.kind
         iconKindStringTextField.text = iconData.kindInChinese
+    }
+    
+    @IBAction func segueToChoseLocationVC() {
+    }
+    
+    @IBAction func unwind(segue:UIStoryboardSegue){
+        //装入地理位置
     }
     
     //完成之后生成一个LocationInfoLocal并且上传
---
+++ igloo/Controllers/AddNewStuffVC/choseLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  choseLocationViewController.swift
+//  igloo
+//
+//  Created by 冯奕琦 on 2019/2/2.
+//  Copyright © 2019 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class ChoseLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
