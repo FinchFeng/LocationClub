33e69b5ba923d679191800bfe2fbdd9bed8fbdbc
FinchFeng <329650437@qq.com>
2019-02-11 13:39:57 +0800

    AirModel完工

---
+++ igloo/Model/ModesForVC/AirModel.swift
@@ -0,0 +1,72 @@
+//
+//  AirModel.swift
+//  igloo
+//
+//  Created by 冯奕琦 on 2019/2/11.
+//  Copyright © 2019 冯奕琦. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class AirModel {
+    
+    //MARK:数据
+    //图标数据类型数据
+    var currentAnnationLocationDataArray:[(id:String,data:LocationInfoRank3)] = []
+    var currentShowingIndexMax = 0
+    
+    //MARK:Functions
+    
+    //获取某个区域的数据并且展示第一Group的数据
+    func getAnnotionsAndShow(span:MKCoordinateRegion,landingBlock:@escaping (([(id:String,data2:LocationInfoRank2,data3:LocationInfoRank3)]))->Void) {
+        currentShowingIndexMax = 0
+        Network.getLocationsIn(span: span) { (data) in
+            //获取array
+            self.currentAnnationLocationDataArray = data
+            self.showNextGroupOfLocationData(landingBlock: landingBlock)
+        }
+    }
+    
+    //使用这个方法来进行数据获取LocationRank3Data
+    func showNextGroupOfLocationData(landingBlock:@escaping (([(id:String,data2:LocationInfoRank2,data3:LocationInfoRank3)]))->Void){
+        let groupAmount = 20
+        //取出要获取的数据
+        let resultDataArray = currentAnnationLocationDataArray[currentShowingIndexMax..<currentShowingIndexMax+groupAmount]
+        currentShowingIndexMax += groupAmount
+        //递归获取这些数据
+        func getData(resultArray:[(id:String,data2:LocationInfoRank2,data3:LocationInfoRank3)],inputArray:[(id:String,data:LocationInfoRank3)]){
+            if let firstData = inputArray.first{
+                //更新输入数据
+                var newInputArray = inputArray
+                newInputArray.remove(at: 0)
+                //获取rank2data
+                var newResultArray = resultArray
+                Network.getLocationInfo(locationID: firstData.id, rank: 2) { (rank2Data) in
+                    let data = rank2Data as! LocationInfoRank2
+                    //更新新的LocationData
+                    newResultArray.append((id:firstData.id , data2: data, data3: firstData.data))
+                    getData(resultArray: newResultArray, inputArray: newInputArray)
+                }
+            }else{
+                //执行landingAction
+                landingBlock(resultArray)
+            }
+        }
+        getData(resultArray: [], inputArray:Array(resultDataArray))
+    }
+    
+    //获取全部数据
+    
+    func getFullData(id:String,landing:@escaping (LocationInfoLocal)->Void) {
+        Network.getLocationInfo(locationID: id, rank: 1) { (data) in
+            landing(data as! LocationInfoLocal)
+        }
+    }
+    
+    
+}
+
+class dataPool{//
+    
+}
